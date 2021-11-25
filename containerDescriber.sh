24a2d003034b593f62ca85255ad3ee838fece2fa
GO <g.owden@gmail.com>
2021-11-25 10:02:50 +0000

    Update containerDescriber.sh

--- containerDescriber.sh
+++ containerDescriber.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to describe all containers in all namesapces cluster
+# Script to describe all containers in all namesapces within a cluster
 
 oc get ns | cut -d " " -f 1 > namespaces.txt
 
@@ -9,9 +9,21 @@ input="namespaces.txt"
 while IFS= read -r namespace 
 do
   if [[ "$namespace" != "NAME" ]]; then
+  
   echo "***************** Looking at $namespace **********************"
   echo "*************************** $namespace ************************" >> ClusterDescription.txt
-  oc get po -n $namespace >> ClusterDescription.txt
+  oc get po -n $namespace | cut -d " " -f 1 > "listOfPods.txt"
+  oc describe ns $namespace >> ClusterDescription.txt
   echo **************************************************************** >> ClusterDescription.txt
+
+  podNames='listOfPods.txt'
+  while read podName; do 
+      if [[ "$podName" != "NAME" ]]; then
+      oc describe po $podName -n $namespace >> ClusterDescription.txt
+  done < "$podNames"
   fi  
+  
+  # Clean up
+  rm -rf listOfPods.txt
+  
 done < "$input"
