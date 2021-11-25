dff8bb102cf2a9ce31446868ac8992b9174df0d4
GO <g.owden@gmail.com>
2021-11-25 09:21:38 +0000

    Add files via upload

---
+++ containerDescriber.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script to describe all containers in all namesapces cluster
+
+oc get ns | cut -d " " -f 1 > namespaces.txt
+
+input="namespaces.txt"
+
+while IFS= read -r namespace 
+do
+  if [[ "$namespace" != "NAME" ]]; then
+  echo "*************************** $namespace ************************" >> ClusterDescription.txt
+  oc get po -n $namespace >> ClusterDescription.txt
+  echo **************************************************************** >> ClusterDescription.txt
+  fi  
+done < "$input"
